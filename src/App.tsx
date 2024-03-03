00e5a558bfcb04582c1975b8444f8c3bfab0b591
hermansapitri00 <hermansapitri00@gmail.com>
2024-03-03 08:24:22 +0700

    fix: about section content and responsive also use icon

--- src/App.tsx
+++ src/App.tsx
@@ -1,12 +1,12 @@
-// import About from "./components/About";
+import About from "./components/About";
 import Hero from "./components/Hero";
 
 function App() {
   return (
-    <>
+    <div className="grid gap-y-6">
       <Hero />
-      {/* <About /> */}
-    </>
+      <About />
+    </div>
   );
 }
 
--- src/components/About/index.tsx
+++ src/components/About/index.tsx
@@ -1,9 +1,29 @@
+import { SiCsharp, SiJavascript, SiHtml5, SiTailwindcss } from "react-icons/si";
+import { FaJava, FaRust } from "react-icons/fa";
+
 export default function About() {
   return (
     <section id="about">
-      <div className="container grid p-2 mx-auto">
-        <div className="text-center" id="text-container"></div>
-        <div id="p-lang-icon-container"></div>
+      <div className="grid p-2 mx-auto gap-y-4 md:container">
+        <div className="place-self-center md:text-center" id="text-container">
+          <h1 className="text-4xl font-extrabold tracking-tight scroll-m-20 lg:text-5xl">
+            About
+          </h1>
+          <p className="leading-7 [&:not(:first-child)]:mdj:mt-4">
+            Lorem ipsum dolor sit amet consectetur, adipisicing elit.
+            Doloremque, reiciendis explicabo! Repellat cupiditate ad explicabo
+            earum incidunt beatae. Pariatur deserunt saepe alias, quos aliquam
+            consequatur soluta laborum laudantium cum magni!
+          </p>
+        </div>
+        <div className="flex flex-wrap items-center justify-center w-full gap-4">
+          <SiCsharp size={"2rem"} />
+          <SiJavascript size={"2rem"} />
+          <SiHtml5 size={"2rem"} />
+          <SiTailwindcss size={"2rem"} />
+          <FaJava size={"2rem"} />
+          <FaRust size={"2rem"} />
+        </div>
       </div>
     </section>
   );
