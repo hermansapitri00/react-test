9c8467555182ceb0403a663c497473549d8abfe9
hermansapitri00 <hermansapitri00@gmail.com>
2024-03-03 08:48:14 +0700

    fix: tailwind config linter

--- tailwind.config.js
+++ tailwind.config.js
@@ -1,11 +1,13 @@
+const tailwindcss_animate = import("tailwindcss-animate");
+
 /** @type {import('tailwindcss').Config} */
-module.exports = {
+const twConfig = {
   darkMode: ["class"],
   content: [
-    './pages/**/*.{ts,tsx}',
-    './components/**/*.{ts,tsx}',
-    './app/**/*.{ts,tsx}',
-    './src/**/*.{ts,tsx}',
+    "./pages/**/*.{ts,tsx}",
+    "./components/**/*.{ts,tsx}",
+    "./app/**/*.{ts,tsx}",
+    "./src/**/*.{ts,tsx}",
   ],
   prefix: "",
   theme: {
@@ -33,5 +35,7 @@ module.exports = {
       },
     },
   },
-  plugins: [require("tailwindcss-animate")],
-}
+  plugins: [tailwindcss_animate],
+};
+
+export default twConfig;
