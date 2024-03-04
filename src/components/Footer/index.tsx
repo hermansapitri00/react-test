f2aa3088f9eecb7b41cc9376fe79e3b2ab7f4d4f
hermansapitri00 <hermansapitri00@gmail.com>
2024-03-04 08:14:20 +0700

    add: footer section

---
+++ src/components/Footer/index.tsx
@@ -0,0 +1,8 @@
+export default function Footer() {
+  return (
+    <footer className="flex flex-col items-center justify-center gap-x-4 md:flex-row md:justify-end">
+      <p className="font-mono text-sm font-semibold">@dendun-nf.github.io</p>
+      <p>Copyright 2022 &copy;</p>
+    </footer>
+  );
+}
