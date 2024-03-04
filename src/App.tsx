1abf6e139179ef37a7ba15e7391d265d1b984aa7
hermansapitri00 <hermansapitri00@gmail.com>
2024-03-04 08:14:42 +0700

    add: components to layout

--- src/App.tsx
+++ src/App.tsx
@@ -1,11 +1,17 @@
 import About from "./components/About";
+import Footer from "./components/Footer";
 import Hero from "./components/Hero";
+import Projects from "./components/Projects";
+import Skills from "./components/Skills";
 
 function App() {
   return (
     <div className="grid gap-y-6">
       <Hero />
       <About />
+      <Skills />
+      <Projects />
+      <Footer />
     </div>
   );
 }
